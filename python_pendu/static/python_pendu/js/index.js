dd39138abc3aef746e21f5aa5d557e4c501e7396
kesank <nkanakelly@hotmail.fr>
2022-04-23 21:21:49 +0200

    keyboard begin

--- python_pendu/static/python_pendu/js/index.js
+++ python_pendu/static/python_pendu/js/index.js
@@ -1,1 +1,26 @@
-console.log(589999)
+console.log(5899)
+console.log("la vue est chaude")
+
+let tab=["a",'b','c','d','e','f','g'];
+let key = document.getElementById('keyboard')
+let word= document.getElementById('word')
+let cont=word.innerHTML
+cont=cont.toUpperCase()
+let arr=cont.split('')
+console.log(cont)
+
+function shuffleArray(inputArray){
+    inputArray.sort(()=> Math.random() - 0.5);
+}
+
+
+shuffleArray(arr);
+
+function ajoutElementTexte(texte){
+
+  key.innerHTML +="<button class='btn btn-primary'>"+texte+"</button>";
+}
+
+for(let i = 0;i<arr.length;i++){
+   ajoutElementTexte(arr[i]);
+}
