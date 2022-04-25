919e405e2a5b674115a55310b110a537a67a2da4
m4rchsp <jmarcelo.afonso@gmail.com>
2022-04-25 10:23:56 +0200

    20220425.1023 M4rch

--- dictionary/models.py
+++ dictionary/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+# from django.contrib.auth import authenticate
+
+
 
 # Create your models here.
+
+# class User():
+#
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Dictionary_t(models.Model):
+    word= models.CharField(max_length=30)
+    classification = models.CharField(max_length=10)
+    definition = models.CharField(max_length=100)
+    url_image = models.URLField(max_length=50, blank=True, default="")
+    frequency = models.IntegerField(blank=True, default=0)
+
+    def __str__(self):
+        return str(self.word)+str(self.definition)
+
+
+class History_t(models.Model):
+    duration = models.FloatField(default=None) # time to find the word
+    created_at = models.DateField() # date of the game
+    word = models.ForeignKey(Dictionary_t, on_delete=models.PROTECT) #
+    #username = models.ForeignKey(user.models.username, on_delete=models.PROTECT) # gamer's name
+
+    def __str__(self):
+        return self.word
+
+    # class Meta:
+    #     ordering = ['batiment', 'piece']
+    #     verbose_name_plural = "Bureaux"
