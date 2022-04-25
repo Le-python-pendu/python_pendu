bd7a65125ed29bd62b901b3612e4550b10b04e50
kesank <nkanakelly@hotmail.fr>
2022-04-25 02:35:58 +0200

    word in html

--- python_pendu/static/python_pendu/js/index.js
+++ python_pendu/static/python_pendu/js/index.js
@@ -1,10 +1,19 @@
 console.log(5899)
 console.log("la vue est chaude")
 let word= document.getElementById('word')
+let pendu= document.getElementById('pendu')
 let cont=word.innerHTML
 cont=cont.toUpperCase()
 let arr=cont.split('')
 let verif=[].concat(arr)
+let arr_replace=[]
+for(let i=0;i<verif.length;i++){
+    arr_replace.push('_')
+}
+let newArr=[].concat(arr_replace)
+arr_replace=arr_replace.join(" ")
+console.log(arr_replace)
+pendu.innerHTML=arr_replace
 let tab=["a",'b','c','d','e','f','g'];
 tab=tab.join('')
 tab=tab.toUpperCase()
@@ -12,6 +21,7 @@ tab=tab.split('')
 let key = document.getElementById('keyboard')
 
 let countEr=0
+//let finPendu=[]
 
 Array.prototype.push.apply(arr, tab);
 console.log(arr)
@@ -26,7 +36,13 @@ function addKey(texte){
     console.log(texte)
   key.innerHTML +="<button class='evClick btn btn-primary'>"+texte+"</button>";
 }
-
+function getAllIndexes(arr, val) {
+    var indexes = [], i;
+    for(i = 0; i < arr.length; i++)
+        if (arr[i] === val)
+            indexes.push(i);
+     return indexes
+}
 for(let i = 0;i<arr.length;i++){
    addKey(arr[i]);
 }
@@ -50,10 +66,25 @@ for(let i = 0;i<but.length;i++){
 
         }
         else{
+            let  finPendu=getAllIndexes(verif,resp)
+            console.log(finPendu.length)
+            for(let i =0; i<finPendu.length;i++){
+                newArr[finPendu[i]]=resp
+                console.log(resp)
+                console.log(newArr[finPendu[i]])
+                console.log(finPendu[i])
+
+            }
+
+
             console.log('Vrai')
+
         }
+        console.log(newArr)
+        pendu.innerHTML=newArr.join(' ')
 		console.log(event.target.innerText)
 	}, false);
 
 }
+
 
