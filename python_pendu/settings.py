3b12aa8dbe30fcc132a2452c23a0bfe33e98d6da
m4rchsp <jmarcelo.afonso@gmail.com>
2022-04-27 00:20:29 +0200

    deploy to heroku

--- python_pendu/settings.py
+++ python_pendu/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -136,3 +136,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'python_pendu', 'static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'connexion'
 LOGIN_REDIRECT_URL = 'home'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
